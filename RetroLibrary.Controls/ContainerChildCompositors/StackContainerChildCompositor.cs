6e44f8366a999e6af35b9ef595d8546b0698b50a
Nick Pateman <nick@npsoftware.co.uk>
2025-11-30 14:48:46 +0000

    feat: stack compositor with horizontal and vertical options, needs spacing / padding

---
+++ RetroLibrary.Controls/ContainerChildCompositors/StackContainerChildCompositor.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using RetroLibrary.Controls.Interfaces;
+using RetroLibrary.Core.Base;
+using RetroLibrary.Core.Enums;
+
+namespace RetroLibrary.Controls.ContainerChildCompositors;
+
+public class StackContainerChildCompositor : IContainerChildCompositor
+{
+    public RetroSpriteContainer? ParentContainer { get; private set; }
+
+    public Direction Direction { get; set; } = Direction.Vertical;
+
+    public Point GetChildPosition(RetroSpriteBase retroSpriteBase)
+    {
+        if (ParentContainer == null)
+        {
+            return Point.Zero;
+        }
+
+        var currentIndex = ParentContainer!.Children.IndexOf(retroSpriteBase);
+        if (currentIndex == -1)
+        {
+            return Point.Zero;
+        }
+
+        var offset = 0;
+        for (var i = 0; i < currentIndex; i++)
+        {
+            offset += Direction == Direction.Vertical
+                ? ParentContainer!.Children[i].Size.Y
+                : ParentContainer!.Children[i].Size.X;
+        }
+
+        return Direction == Direction.Vertical
+            ? new Point(0, offset)
+            : new Point(offset, 0);
+    }
+
+    public void SetParentContainer(RetroSpriteContainer parentContainer)
+    {
+        ParentContainer = parentContainer;
+    }
+}
---
+++ RetroLibrary.Core/Enums/Direction.cs
@@ -0,0 +1,7 @@
+﻿namespace RetroLibrary.Core.Enums;
+
+public enum Direction
+{
+    Horizontal = 1,
+    Vertical = 2
+}
